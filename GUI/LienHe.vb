905919103fbffaad4ed1adb7db460e2fd5f540e0
Nam Le <lehoangnamm1997@gmail.com>
2018-04-27 16:37:45 +0700

    push an existing project

---
+++ GUI/LienHe.vb
@@ -0,0 +1,5 @@
+﻿Public Class LienHe
+    Protected Overrides Sub OnLoad(e As EventArgs)
+
+    End Sub
+End Class
---
+++ PhieuBanSach/Form1.vb
@@ -0,0 +1,56 @@
+﻿Public Class Form1
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If (CheckBox1.CheckState = CheckState.Checked) Then
+            txtNgayLap.ReadOnly = True
+            txtNgayLap.Enabled = True
+        Else
+            txtNgayLap.ReadOnly = False
+            txtNgayLap.Enabled = False
+        End If
+    End Sub
+
+    Private Sub btnAddKH_Click(sender As Object, e As EventArgs) Handles btnAddKH.Click
+        txtHoTen.ReadOnly = True
+        txtEmail.ReadOnly = True
+        txtDiaChi.ReadOnly = True
+        txtSDT.ReadOnly = True
+        txtTienNo.Text = "0"
+
+    End Sub
+
+    Private Sub txtTienNo_TextChanged(sender As Object, e As EventArgs) Handles txtTienNo.TextChanged
+        If (txtTienNo.Text = "") Then
+            txtTienNo.ForeColor = Color.White
+        Else
+            If (Int32.Parse(txtTienNo.Text) < 20000) Then
+                txtTienNo.ForeColor = Color.Green
+            Else
+                txtTienNo.ForeColor = Color.Red
+
+            End If
+        End If
+
+    End Sub
+
+
+
+    Private Sub btnRefresh_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
+        txtHoTen.ReadOnly = False
+        txtHoTen.Text = ""
+        txtEmail.ReadOnly = False
+        txtEmail.Text = ""
+        txtDiaChi.ReadOnly = False
+        txtDiaChi.Text = ""
+        txtSDT.ReadOnly = False
+        txtSDT.Text = ""
+        txtTienNo.Text = "0"
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        'add Data
+
+
+        btnRefresh_Click(sender, e)
+
+    End Sub
+End Class
